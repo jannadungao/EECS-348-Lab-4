c6fcb882ee2ccf2e857fdd69bcb7cf34a6c1902e
jannadungao <jannadungao2@gmail.com>
2024-10-02 13:57:38 -0500

    Update problem1.c

--- problem1.c
+++ problem1.c
@@ -2,15 +2,15 @@
 
 int main() {
     // initialize
-    int score = 0;
+    int score = 2;
 
     while (score > 1) { // continue loop until user enters a score less than or equal to 1
         // take input and store in variable
         printf("Enter 0 or 1 to STOP\n");
-        printf("Enter the NFL score: \n");
+        printf("Enter the NFL score: ");
         scanf("%d", &score);
 
-        // initializing plays variables 
+        // keeps track of how many times a play is done
         int TD = 0; // 6pts
         int FG = 0; // 3pts
         int safety = 0; // 2pts
@@ -18,28 +18,15 @@ int main() {
         int TD_FG = 0; // 7pts
         int temp_score = score;
 
-        // calculate possible combinations
-        while (temp_score > 0) {
-            if (temp_score >= 8) {
-                TD_2PT++;
-                temp_score -= 8;
-            } else if (temp_score >= 7) {
-                TD_FG++;
-                temp_score -= 7;
-            } else if (temp_score >= 6) {
-                TD++;
-                temp_score -= 6;
-            } else if (temp_score >= 3) {
-                FG++;
-                temp_score -= 3;
-            } else if (temp_score >= 2) {
-                safety++;
-                temp_score -= 2;
-            }
-        }
-
-        printf("%d TD + 2PT, %d TD + FG, %d TD, %d 3pt FG, %d Safety\n", TD_2PT, TD_FG, TD, FG, safety);
+        // calculate max times each play can happen for the given score
+        int TD_max = temp_score / 6;
+        int FG_max = temp_score / 3;
+        int safety_max = temp_score / 2;
+        int TD_2PT_max = temp_score / 8;
+        int TD_FG_max = temp_score / 7;
 
 
+        // calculate possible permutations
+    
     }
 }
