82f4623e35cf8ee4297d643377449b03323c8891
jannadungao <jannadungao2@gmail.com>
2024-10-02 13:03:47 -0500

    Update problem1.c

---
+++ problem1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    // initialize and take input
+    int score;
+    printf("Enter 0 or 1 to STOP\n");
+    printf("Enter the NFL score: \n");
+    scanf("%d", &score);
+
+    
+}
