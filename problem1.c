dbe04ca8f66cd50c99e0a1c8ab7837da6e45cdb6
jannadungao <jannadungao2@gmail.com>
2024-10-02 14:13:43 -0500

    Final

--- problem1.c
+++ problem1.c
@@ -4,12 +4,14 @@ int main() {
     // initialize
     int score = 2;
 
-    while (score > 1) { // continue loop until user enters a score less than or equal to 1
+    while (1) { // continue loop until user enters a score less than or equal to 1
         // take input and store in variable
         printf("Enter 0 or 1 to STOP\n");
         printf("Enter the NFL score: ");
         scanf("%d", &score);
-
+        if (score <= 1) {
+            break;
+        }
         // keeps track of how many times a play is done
         int TD = 0; // 6pts
         int FG = 0; // 3pts
